541b513cbd4a8bd28d97f9b485b67c462dec12ad
ANIRUDH SINGH <anirudh8400@gmail.com>
2022-08-02 16:53:13 +0530

    first commit

---
+++ webappifordatafetch/webappifordatafetch/Migrations/20220801054946_employee.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webappifordatafetch.Migrations
+{
+    public partial class employee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Employee");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
